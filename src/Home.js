4e55b2f4ea6f0e8070e2f9a30fbc1d5a9152cc77
B12Ly <Chabil.Sat@gmail.com>
2025-02-09 20:19:41 +0700

    home

--- src/Home.js
+++ src/Home.js
@@ -3,11 +3,7 @@ import React from 'react';
 import Footer from './Footer'; // นำเข้า Footer.js
 
 function Home() {
-  return (
-    <div className="home-container">
-      <Footer /> {/* แสดง Footer ในหน้า Home */}
-    </div>
-  );
+  
 }
 
 export default Home;
